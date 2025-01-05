ab866297406cf6c7409ba5c690464e6650b4346f
AkaakuHub <105225777+github-actions@users.noreply.github.com>
2025-01-05 16:23:55 +0900

    chore: postページ

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -7,6 +7,7 @@ import { useRouter, redirect } from "next/navigation";
 import { IconHeart, IconMessageCircle, IconShare } from "@tabler/icons-react";
 import { ExtendedSession } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   const { data: session, status } = useSession();
@@ -90,61 +91,65 @@ const Home = () => {
         {mockData.posts.map(post => {
           const user = getUserById(post.userId);
           return (
-            <div
+            <Link
+              href={`/posts/${user?.id}`}
               key={post.id}
-              className="border-b border-gray-200 py-4 px-4 flex gap-4"
             >
-              <Image
-                src={user?.image || "no_avatar_image_128x128.png"}
-                alt={user?.name || "User"}
-                className="w-12 h-12 rounded-full"
-                width={48}
-                height={48}
-              />
-              <div className="flex-1">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <span className="font-bold">
-                      {user?.name || "Unknown User"}
-                    </span>
-                    <span className="text-sm text-gray-500 ml-2">
-                      @{user?.id || "unknown"}
+              <div
+                className="border-b border-gray-200 py-4 px-4 flex gap-4"
+              >
+                <Image
+                  src={user?.image || "no_avatar_image_128x128.png"}
+                  alt={user?.name || "User"}
+                  className="w-12 h-12 rounded-full"
+                  width={48}
+                  height={48}
+                />
+                <div className="flex-1">
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <span className="font-bold">
+                        {user?.name || "Unknown User"}
+                      </span>
+                      <span className="text-sm text-gray-500 ml-2">
+                        @{user?.id || "unknown"}
+                      </span>
+                    </div>
+                    <span className="text-xs text-gray-400">
+                      {new Date(post.createdAt).toLocaleString()}
                     </span>
                   </div>
-                  <span className="text-xs text-gray-400">
-                    {new Date(post.createdAt).toLocaleString()}
-                  </span>
-                </div>
-                <p className="mt-1 text-gray-800">{post.content}</p>
-                {post.images && (
-                  <div className="mt-2 grid grid-cols-2 gap-2">
-                    {post.images.map((image, index) => (
-                      <Image
-                        key={index}
-                        src={image}
-                        alt="Post Image"
-                        className="w-full h-auto rounded-md"
-                        width={200}
-                        height={200}
-                      />
-                    ))}
+                  <p className="mt-1 text-gray-800">{post.content}</p>
+                  {post.images && (
+                    <div className="mt-2 grid grid-cols-2 gap-2">
+                      {post.images.map((image, index) => (
+                        <Image
+                          key={index}
+                          src={image}
+                          alt="Post Image"
+                          className="w-full h-auto rounded-md"
+                          width={200}
+                          height={200}
+                        />
+                      ))}
+                    </div>
+                  )}
+                  <div className="flex items-center gap-8 mt-2 text-gray-500">
+                    <button className="flex items-center gap-1 hover:text-red-500 w-[calc(100% / 3)] justify-center">
+                      <IconHeart size={20} />
+                      <span>10</span>
+                    </button>
+                    <button className="flex items-center gap-1 hover:text-blue-500 w-[calc(100% / 3)] justify-center">
+                      <IconMessageCircle size={20} />
+                      <span>20</span>
+                    </button>
+                    <button className="flex items-center gap-1 hover:text-green-500 w-[calc(100% / 3)] justify-center">
+                      <IconShare size={20} />
+                    </button>
                   </div>
-                )}
-                <div className="flex items-center gap-8 mt-2 text-gray-500">
-                  <button className="flex items-center gap-1 hover:text-red-500 w-[calc(100% / 3)] justify-center">
-                    <IconHeart size={20} />
-                    <span>10</span>
-                  </button>
-                  <button className="flex items-center gap-1 hover:text-blue-500 w-[calc(100% / 3)] justify-center">
-                    <IconMessageCircle size={20} />
-                    <span>20</span>
-                  </button>
-                  <button className="flex items-center gap-1 hover:text-green-500 w-[calc(100% / 3)] justify-center">
-                    <IconShare size={20} />
-                  </button>
                 </div>
               </div>
-            </div>
+            </Link>
           );
         })}
       </div>
